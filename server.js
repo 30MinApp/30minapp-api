ae4f33cd403df16448012352e4f9fdf9d9d1a07b
Kevin-Bateman <kbateman.work@gmail.com>
2020-02-20 10:07:49 -0700

    added cors to api

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express")
+const cors = require("cors")()
 const app = express()
+app.use(cors)
 app.get("/persons/:id", (req, res, next) => {
     if (parseInt(req.params["id"]) !== 1) {
         return next("person not found")
