fbade57ef8ea343bf146d450d2ff30e6784f9a8d
Kevin-Bateman <kbateman.work@gmail.com>
2020-02-20 10:03:01 -0700

    created api

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+app.get("/persons/:id", (req, res, next) => {
+    if (parseInt(req.params["id"]) !== 1) {
+        return next("person not found")
+    }
+    const data = {
+        id: 1,
+        name: "Kevin Bateman",
+        bday: "1994-10-11"
+    }
+    res.json(data)
+})
+app.listen(process.env.PORT || 8080)
